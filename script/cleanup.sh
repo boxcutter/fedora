b93a093ad91d7c98aa29c11617b5653647975556
Mischa Taylor <mischa@misheska.com>
2014-05-13 10:57:13 -0700

    Initial migration from http://github.com/misheska/basebox-packer to new form

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -4,3 +4,9 @@ echo "==> Cleaning up yum cache of metadata and packages to save space"
 yum -y clean all
 
 rm -rf /tmp/*
+
+echo "==> Zeroing out empty area to save space in the final image"
+# Zero out the free space to save space in the final image.  Contiguous
+# zeroed space compresses down to nothing.
+dd if=/dev/zero of=/EMPTY bs=1M
+rm -f /EMPTY
