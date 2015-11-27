506195096c0e2432c5960f90159f8d9bfa208d91
Moujan Anna <moujan@annawake.com>
2015-11-26 21:05:45 -0800

    Make images smaller by delaying creation of swap partition

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -3,10 +3,38 @@
 echo "==> Cleaning up ${PKG_MGR} cache of metadata and packages to save space"
 ${PKG_MGR} -y clean all
 
+echo "==> Removing temporary files used to build box"
 rm -rf /tmp/*
+
+echo '==> Clear out swap and disable until reboot'
+set +e
+swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
+case "$?" in
+	2|0) ;;
+	*) exit 1 ;;
+esac
+set -e
+if [ "x${swapuuid}" != "x" ]; then
+    # Whiteout the swap partition to reduce box size
+    # Swap is disabled till reboot
+    swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
+    /sbin/swapoff "${swappart}"
+    dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
+    /sbin/mkswap -U "${swapuuid}" "${swappart}"
+fi
 
 echo "==> Zeroing out empty area to save space in the final image"
 # Zero out the free space to save space in the final image.  Contiguous
 # zeroed space compresses down to nothing.
-dd if=/dev/zero of=/EMPTY bs=1M
+dd if=/dev/zero of=/EMPTY bs=1M || echo "dd exit code $? is suppressed"
 rm -f /EMPTY
+
+# Block until the empty file has been removed, otherwise, Packer
+# will try to kill the box while the disk is still full and that's bad
+sync
+
+echo "==> Disk usage before cleanup"
+echo ${DISK_USAGE_BEFORE_CLEANUP}
+
+echo "==> Disk usage after cleanup"
+df -h
