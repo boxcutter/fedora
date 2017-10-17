777d5f7616528db054ffb38fe04838779900c2af
Patrick M. Slattery <pslattery@mywebgrocer.com>
2017-10-17 18:49:20 -0400

    Add Fedora 26/27 support to the VirtualBox guest additions install script

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -5,15 +5,15 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
-    dnf -y install kernel-headers-$(uname -r) kernel-devel-$(uname -r) gcc make perl
-
+    # Some of these packages should already have been installed in the kickstart
+    dnf -y install kernel-headers-"$(uname -r)" kernel-devel-"$(uname -r)" gcc make perl
+    # Need to set the KERN_DIR in Fedora 26 and 27 or the VBox additions will not install
+    /usr/libexec/system-python -mplatform | grep -qi "fedora-2[67]" && echo "Fedora 26 or 27 detected" && \
+    KERN_DIR=/usr/src/kernels/"$(uname -r)" && export KERN_DIR
     VBOX_VERSION=$(cat $SSH_USER_HOME/.vbox_version)
     mount -o loop $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso /mnt
     sh /mnt/VBoxLinuxAdditions.run --nox11
     umount /mnt
     rm -rf $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso
     rm -f $SSH_USER_HOME/.vbox_version
-
-    echo "==> Removing packages needed for building guest tools"
-    dnf -y remove gcc cpp kernel-devel kernel-headers perl
 fi
