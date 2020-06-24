413f26641b96f0a0c4ddb77ae7347e5eef52288c
Pysis868 <Pysis868@Gmail.com>
2020-06-24 19:16:21 -0400

    Fixing python references and comment. Not sure when this ever worked.

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -6,9 +6,9 @@ SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
     echo "==> Installing VirtualBox guest additions"
     # Need to set the KERN_DIR in Fedora 26 and 27 or the VBox additions will not install
-    /usr/libexec/system-python -mplatform | grep -qi "fedora-2[67]" && echo "Fedora 26 or 27 detected" && \
+    python -mplatform | grep -qi "fedora-2[67]" && echo "Fedora 26 or 27 detected" && \
         KERN_DIR=/usr/src/kernels/"$(uname -r)" && export KERN_DIR
-    /usr/libexec/system-python -mplatform | grep -qi "fedora-28" && echo "Fedora 28 detected" && \
+    python -mplatform | grep -qi "fedora-28" && echo "Fedora 28 detected" && \
         KERN_DIR=/lib/modules/"$(uname -r)"/build && export KERN_DIR
     if [ "x$KERN_DIR" != "x" ]; then
         # Some of these packages should already have been installed in the kickstart
@@ -18,7 +18,7 @@ if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
         sh /mnt/VBoxLinuxAdditions.run --nox11
         umount /mnt
     else
-        echo "Couldn't identify Fedora between 26 and 28: couldn't set KERN_DIR."
+        echo "Couldn't identify Fedora between 26/27 and 28: couldn't set KERN_DIR."
         echo "Canceling installation of VirtualBox guest additions"
     fi
     rm -rf $SSH_USER_HOME/VBoxGuestAdditions_$VBOX_VERSION.iso
